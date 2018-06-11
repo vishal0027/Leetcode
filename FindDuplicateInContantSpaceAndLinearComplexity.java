165130dcd624e377ccfc4782129bc2f9a6ac0e89
Vishal Malik <vishal27@my.yorku.ca>
2018-06-11 16:04:23 -0400

    Added FindDuplicateInContantSpaceAndLinearComplexity

---
+++ FindDuplicateInContantSpaceAndLinearComplexity.java
@@ -0,0 +1,13 @@
+public class FindDuplicateInContantSpaceAndLinearComplexity{
+	public FindDuplicateInContantSpaceAndLinearComplexity(){}
+	public FindDuplicateInContantSpaceAndLinearComplexity(int input[]){
+		findIt(input);
+	}
+	public void findIt(int input[]){ 
+        System.out.println("The repeating elements are : ");
+        for (int i = 0; i < input.length; i++){
+            if (input[Math.abs(input[i])] >= 0) input[Math.abs(input[i])] = -input[Math.abs(input[i])];
+            else System.out.print(Math.abs(input[i]) + " ");
+        } 
+	}
+}
