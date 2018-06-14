46d4c21f83a86f8a8b95b9c2d5256509028e47aa
Vishal Malik <vishal27@my.yorku.ca>
2018-06-14 09:56:20 -0400

    Formated

--- FindDuplicateInContantSpaceAndLinearComplexity.java
+++ FindDuplicateInContantSpaceAndLinearComplexity.java
@@ -4,10 +4,10 @@ public class FindDuplicateInContantSpaceAndLinearComplexity{
 		findIt(input);
 	}
 	public void findIt(int input[]){ 
-        System.out.println("The repeating elements are : ");
-        for (int i = 0; i < input.length; i++){
-            if (input[Math.abs(input[i])] >= 0) input[Math.abs(input[i])] = -input[Math.abs(input[i])];
-            else System.out.print(Math.abs(input[i]) + " ");
-        } 
+		System.out.println("The repeating elements are : ");
+		for (int i = 0; i < input.length; i++){
+		    if (input[Math.abs(input[i])] >= 0) input[Math.abs(input[i])] = -input[Math.abs(input[i])];
+		    else System.out.print(Math.abs(input[i]) + " ");
+		} 
 	}
 }
